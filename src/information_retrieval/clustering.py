8a713e5659772a84174891fa00d97bb987e7a3e7
GeorgePark <george--park@hotmail.com>
2017-10-17 15:19:06 +0200

    Clustering on target papers works
    Clustering now works on the papers in the database thanks to help from
    Melroy

--- src/information_retrieval/clustering.py
+++ src/information_retrieval/clustering.py
@@ -1,24 +1,26 @@
+import time
+
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.cluster import KMeans
+from import_data import database
+import cleanup_module.cleanup
+
 
 # Need the Tf-idf-weighted document-term matrix to use KMeans
 from information_retrieval.indexer import Indexer, paper_fields
 
-documents = ["Human machine interface for lab abc computer applications",
-             "A survey of user opinion of computer system response time",
-             "The EPS user interface management system",
-             "System and human system engineering testing of EPS",
-             "Relation of user perceived response time to error measurement",
-             "The generation of random binary unordered trees",
-             "The intersection graph of paths in trees",
-             "Graph minors IV Widths of trees and well quasi ordering",
-             "Graph minors A survey"]
+cleanup_module.cleanup.get_cleanup_instance(database).clean(database.papers)
+
+documents = [paper.paper_text for paper in database.papers]
 
 #vectorize the text i.e. convert the strings to numeric features
+start = time.time()
 vectorizer = TfidfVectorizer(stop_words='english')
 X = vectorizer.fit_transform(documents)
+print(time.time() - start)
 
 #cluster documents
+start = time.time()
 true_k = 2
 model = KMeans(n_clusters=true_k, init='k-means++', max_iter=100, n_init=1)
 model.fit_transform(X)
@@ -32,3 +34,4 @@ for i in range(true_k):
     for ind in order_centroids[i, :10]:
         print (' %s' % terms[ind],)
     print ('\n')
+print(time.time() - start)
